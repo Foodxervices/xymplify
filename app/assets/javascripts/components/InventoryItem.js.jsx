37e3d3f601d11b63c54d59c8f151bf5e45814c63
James La <jamesla0604@gmail.com>
2016-09-08 12:01:23 +0700

    update InventoryItem.js.jsx

--- app/assets/javascripts/components/InventoryItem.js.jsx
+++ app/assets/javascripts/components/InventoryItem.js.jsx
@@ -6,17 +6,7 @@ const InventoryItem = React.createClass({
     this.updateCurrentQuantity(this.props.current_quantity + 1)
   },
   updateCurrentQuantity: function(nextCurrentQuantity) {
-    $.ajax({
-      type: 'PATCH',
-      url: `/inventories/${this.props.id}/update_current_quantity`,
-      data: { food_item: { current_quantity: nextCurrentQuantity } },
-      success: (food_item) => {
-        this.props.onCurrentQuantityChange(food_item.id, food_item.current_quantity)
-      }
-    });
-  },
-  onCurrentQuantityChange: function(event) {
-    let nextCurrentQuantity = parseInt(event.target.value) || 0
+    nextCurrentQuantity = parseInt(nextCurrentQuantity) || 0
 
     this.props.onCurrentQuantityChange(this.props.id, nextCurrentQuantity)
 
@@ -26,8 +16,15 @@ const InventoryItem = React.createClass({
     }
 
     this.updateCurrentQuantityTimeOut = setTimeout(() => {
-      this.updateCurrentQuantity(this.refs.currentQuantity.value)
-    }, 1000)
+      $.ajax({
+        type: 'PATCH',
+        url: `/inventories/${this.props.id}/update_current_quantity`,
+        data: { food_item: { current_quantity: nextCurrentQuantity } },
+        success: (food_item) => {
+          this.props.onCurrentQuantityChange(food_item.id, food_item.current_quantity)
+        }
+      });
+    }, 1000)  
   },
   render: function() {
     const { name, supplier_name, current_quantity, quantity_ordered, unit, unit_price } = this.props
@@ -39,7 +36,7 @@ const InventoryItem = React.createClass({
          <td className="current-quantity">
             <div className="form-inline">
               <a className="btn btn-minus form-control" onClick={this.minusQuantity}>-</a>
-              <input ref='currentQuantity' className="form-control" type="number" value={current_quantity} onChange={this.onCurrentQuantityChange}/>
+              <input ref='currentQuantity' className="form-control" type="number" value={current_quantity} onChange={(event) => this.updateCurrentQuantity(event.target.value)}/>
               <a className="btn btn-plus form-control" onClick={this.plusQuantity}>+</a>
             </div>
          </td>
