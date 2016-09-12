f88be5832a2d9e9a3190a6edcb46e0cad7844282
James La <jamesla0604@gmail.com>
2016-09-12 20:27:21 +0700

    update Sortable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,6 @@ module ApplicationHelper
     title ||= kclass.human_attribute_name(column)
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+    link_to title, params.merge(:sort => column, :direction => direction), {:class => css_class}
   end
 end
