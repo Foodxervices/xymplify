9480b0467857fb5c0f256821e27adcc8f3d20e2c
James La <jamesla0604@gmail.com>
2016-09-13 08:43:45 +0700

    update filter and supplier

--- app/services/supplier_filter.rb
+++ app/services/supplier_filter.rb
@@ -8,7 +8,15 @@ class SupplierFilter
 
   def result
     suppliers = Supplier.all
-    suppliers = suppliers.where("suppliers.name ILIKE :keyword", keyword: "%#{keyword}%") if keyword.present?
+    suppliers = suppliers.where("
+                            suppliers.name      ILIKE :keyword OR
+                            suppliers.address   ILIKE :keyword OR
+                            suppliers.country   ILIKE :keyword OR
+                            suppliers.contact   ILIKE :keyword OR
+                            suppliers.telephone ILIKE :keyword OR
+                            suppliers.email     ILIKE :keyword OR
+                            suppliers.currency  ILIKE :keyword 
+                          ", keyword: "%#{keyword}%") if keyword.present?
     suppliers
   end
 
