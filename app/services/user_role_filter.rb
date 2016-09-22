1b77626e629f1dcb2f9fa4e56d72ce9fc9e28f20
James La <jamesla0604@gmail.com>
2016-09-22 09:03:18 +0700

    update UserRoleFilter

--- app/services/user_role_filter.rb
+++ app/services/user_role_filter.rb
@@ -9,12 +9,13 @@ class UserRoleFilter
   def result
     user_roles =  UserRole.all
     user_roles =  user_roles.joins("LEFT JOIN users ON user_roles.user_id = users.id")
-                            .joins("LEFT JOIN restaurants ON user_roles.restaurant_id = restaurants.id")
+                            .joins("LEFT JOIN kitchens_user_roles ON user_roles.id = kitchens_user_roles.user_role_id")
+                            .joins("LEFT JOIN kitchens ON kitchens.id = kitchens_user_roles.kitchen_id")
                             .joins("LEFT JOIN roles ON user_roles.role_id = roles.id")
                             .where("
                                     roles.name        ILIKE :keyword OR
                                     users.name        ILIKE :keyword OR
-                                    restaurants.name  ILIKE :keyword
+                                    kitchens.name     ILIKE :keyword
                                   ", keyword: "%#{keyword}%") if keyword.present?
     user_roles
   end
