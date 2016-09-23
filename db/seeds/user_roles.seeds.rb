a38eae4cef321acfc8e2a2600ce56ae406108322
James La <jamesla0604@gmail.com>
2016-09-23 09:58:23 +0700

    update seed

--- db/seeds/user_roles.seeds.rb
+++ db/seeds/user_roles.seeds.rb
@@ -11,7 +11,6 @@ after [:roles, :users, :restaurants] do
   aihue   = Restaurant.find_by_name('Ai Hue')
   UserRole.create([
     { role: Role.find_by_name('Owner'), user: User.find_by_name('Windsor Owner'), restaurant: windsor},
-    { role: Role.find_by_name('Owner'), user: User.find_by_name('Windsor Manager'), restaurant: windsor, kitchens: [windsor.kitchens.first]},
     { role: Role.find_by_name('Kitchen Manager'), user: User.find_by_name('Windsor Manager'), restaurant: windsor, kitchens: [windsor.kitchens.last]},
     { role: Role.find_by_name('Owner'), user: User.find_by_name('Ai Hue Owner'), restaurant: aihue},
     { role: Role.find_by_name('Kitchen Manager'), user: User.find_by_name('Ai Hue Manager'), restaurant: aihue, kitchens: [aihue.kitchens.first]},
