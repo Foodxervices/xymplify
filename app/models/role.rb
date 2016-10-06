8ca8f0722fd3f6c17a6c0f792a0784bdbd396fb8
James La <jamesla0604@gmail.com>
2016-10-06 12:28:25 +0700

    remove kitchen::read role

--- app/models/role.rb
+++ app/models/role.rb
@@ -9,7 +9,7 @@ class Role < ActiveRecord::Base
     'supplier__manage', 'supplier__read', 'supplier__create', 'supplier__update', 'supplier__destroy',
     'user_role__manage', 'user_role__read', 'user_role__create', 'user_role__update', 'user_role__destroy', 
     'order__read', 'order__mark_as_shipped', 'order__mark_as_cancelled',
-    'kitchen__import', 'kitchen__read',
+    'kitchen__import', 
     'food_item__manage', 'food_item__read', 'food_item__create', 'food_item__update', 'food_item__destroy', 'food_item__update_current_quantity', 'food_item__order'
   ], multiple: true
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -9,7 +9,7 @@ describe Role do
         'restaurant__manage', 'restaurant__read', 'restaurant__create', 'restaurant__update', 'restaurant__destroy', 
         'supplier__manage', 'supplier__read', 'supplier__create', 'supplier__update', 'supplier__destroy',
         'user_role__manage', 'user_role__read', 'user_role__create', 'user_role__update', 'user_role__destroy',
-        'kitchen__import', 'kitchen__read',
+        'kitchen__import', 
         'order__read', 'order__mark_as_shipped', 'order__mark_as_cancelled'
       ) 
     end
