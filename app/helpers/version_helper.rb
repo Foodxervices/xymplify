afc9aeef9b2c78f2f6886ec10d7966d83933ffce
James La <jamesla0604@gmail.com>
2016-10-14 10:58:39 +0700

    update version_value

--- app/helpers/version_helper.rb
+++ app/helpers/version_helper.rb
@@ -1,6 +1,6 @@
 module VersionHelper
   def version_value(field, value) 
-    if ['kitchen_id', 'supplier_id', 'user_id', 'role_id', 'restaurant_id', 'user_role_id'].include?(field)
+    if ['kitchen_id', 'supplier_id', 'user_id', 'role_id', 'restaurant_id', 'user_role_id', 'category_id'].include?(field)
       field = field.gsub("_id", "")
 
       kclass = field.classify.constantize
@@ -8,8 +8,12 @@ module VersionHelper
       object = kclass.find_by_id(value)
 
       if object.present?
-        object = object.becomes(User) if object.kind_of?(Admin)
-        value = link_to_if(can?(:show, object), object&.name, object, remote: true) 
+        if ['category_id'].includes?(field)
+          value = object&.name
+        else
+          object = object.becomes(User) if object.kind_of?(Admin)
+          value = link_to_if(can?(:show, object), object&.name, object, remote: true) 
+        end
       end
     end
 
