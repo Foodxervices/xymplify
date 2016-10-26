02e5d9c58ad4d13a7c903865d0769089b3b3b856
James La <jamesla0604@gmail.com>
2016-10-26 14:51:48 +0700

    update OrderItem

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,5 @@
 class OrderItem < ActiveRecord::Base
+  acts_as_taggable
   has_paper_trail
   
   belongs_to :order
@@ -8,6 +9,7 @@ class OrderItem < ActiveRecord::Base
   before_save :cache_restaurant
   before_save :cache_name
   before_save :update_quantity_ordered
+  before_save :set_tags
 
   monetize :unit_price_cents
 
@@ -28,6 +30,10 @@ class OrderItem < ActiveRecord::Base
 
   def cache_name
     self.name = food_item&.name if name.blank?
+  end
+
+  def set_tags
+    self.tags = food_item.tags if food_item.present?
   end
 
   def update_quantity_ordered
