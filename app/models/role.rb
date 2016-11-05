2920832000798612ad0b5ce279dd40389de9d065
James La <jamesla0604@gmail.com>
2016-11-05 19:04:26 +0700

    add Update Accepted PO, Update Declined PO role

--- app/models/role.rb
+++ app/models/role.rb
@@ -8,7 +8,7 @@ class Role < ActiveRecord::Base
     'restaurant__manage', 'restaurant__read', 'restaurant__create', 'restaurant__update', 'restaurant__destroy', 'restaurant__dashboard',
     'supplier__manage', 'supplier__read', 'supplier__create', 'supplier__update', 'supplier__destroy',
     'user_role__manage', 'user_role__read', 'user_role__create', 'user_role__update', 'user_role__destroy', 
-    'order__read', 'order__mark_as_delivered', 'order__mark_as_cancelled', 'order__update_placed', 'order__update_delivered', 'order__update_cancelled',
+    'order__read', 'order__mark_as_delivered', 'order__mark_as_cancelled', 'order__update_placed', 'order__update_accepted', 'order__update_declined', 'order__update_delivered', 'order__update_cancelled',
     'kitchen__import', 
     'food_item__manage', 'food_item__read', 'food_item__create', 'food_item__update', 'food_item__destroy', 'food_item__update_current_quantity', 'food_item__order'
   ], multiple: true
