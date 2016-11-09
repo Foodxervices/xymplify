dddb7a2f97003416f3065d29534782373a0eff49
James La <jamesla0604@gmail.com>
2016-11-09 11:53:22 +0700

    add footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,17 @@
 class HomeController < ApplicationController
-  layout 'public/layouts/main'
-  
+  layout :resolve_layout
+
   def index; end
 
   def retailers; end
+
+  private
+  def resolve_layout
+    case action_name
+    when "index"
+      "public/layouts/home"
+    else
+      "public/layouts/main"
+    end
+  end
 end
