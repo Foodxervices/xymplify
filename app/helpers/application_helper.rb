e7266962ba2ed531cfb9408b7fd6241da497ca1b
James La <jamesla0604@gmail.com>
2016-11-18 17:02:24 +0700

    fix format_price issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -51,6 +51,6 @@ module ApplicationHelper
   end
 
   def format_price(price, currency_code)
-    humanized_money_with_symbol(Money.from_amount(price, currency_code))
+    humanized_money_with_symbol(Money.from_amount(price.to_f, currency_code))
   end
 end
