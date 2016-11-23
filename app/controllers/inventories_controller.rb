17972dd65aa2a0830683d8242a2fe08b4d0d60bb
James La <jamesla0604@gmail.com>
2016-11-23 16:22:00 +0700

    solve n+1 query issue

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -6,7 +6,7 @@ class InventoriesController < ApplicationController
     @food_item_filter = FoodItemFilter.new(@food_items, food_item_filter_params)
     @food_items  = @food_item_filter.result
                                     .where('current_quantity > 0')
-                                    .includes(:supplier, :kitchen)
+                                    .includes(:supplier, :kitchen, :category)
                                     .order(:name, :id)
                                     .paginate(:page => params[:page])
     @groups = {}
