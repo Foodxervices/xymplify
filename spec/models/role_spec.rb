76fcb40df981696b2a65d66a4049336e74b0c2e6
James La <jamesla0604@gmail.com>
2016-12-07 01:00:25 +0700

    add Kitchen Roles

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -9,7 +9,7 @@ describe Role do
         'restaurant__manage', 'restaurant__read', 'restaurant__create', 'restaurant__update', 'restaurant__destroy', 'restaurant__dashboard', 'restaurant__history',
         'supplier__manage', 'supplier__read', 'supplier__create', 'supplier__update', 'supplier__destroy',
         'user_role__manage', 'user_role__read', 'user_role__create', 'user_role__update', 'user_role__destroy',
-        'kitchen__dashboard', 'kitchen__import', 'kitchen__history',
+        'kitchen__create', 'kitchen__update', 'kitchen__dashboard', 'kitchen__import', 'kitchen__history',
         'message__read', 'message__create', 'message__update', 'message__destroy', 
         'order__read', 'order__history', 'order__mark_as_accepted', 'order__mark_as_declined', 'order__mark_as_delivered', 'order__mark_as_cancelled', 'order__update_placed', 'order__update_accepted', 'order__update_declined', 'order__update_delivered', 'order__update_cancelled',
         'inventory__read', 'inventory__update'
