4c6ac67d7f3a2575398bc351cf761cffdf931373
James La <jamesla@Jamess-MacBook-Pro.local>
2017-07-03 17:21:36 +0700

    add missing indexes on taggings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622035800) do
+ActiveRecord::Schema.define(version: 20170703101948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
