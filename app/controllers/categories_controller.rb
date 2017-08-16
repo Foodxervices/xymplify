df5a4cc05e1d332e329a3fcd6863231c5d62f5aa
James La <jamesla0604@gmail.com>
2017-08-16 09:18:36 +0700

    update CategoriesController

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,13 @@
 class CategoriesController < AdminController
+  before_action :init, only: [:index, :by_supplier, :frequently_ordered]
+
   def index
-    init
     @food_items = @food_items.order("category_priority, category_name, supplier_priority, unit_price_cents")
     @groups = @food_items.group_by(&:category_name)
     @hide_category = true
   end
 
   def by_supplier
-    init
     @food_items = @food_items.order("supplier_priority, supplier_name, category_priority, unit_price_cents")
     @groups = @food_items.group_by(&:supplier_name)
     @hide_supplier = true
@@ -15,7 +15,6 @@ class CategoriesController < AdminController
   end
 
   def frequently_ordered
-    init
     @food_items = @food_items.order("ordered_count desc, supplier_priority, category_priority")
     @groups = { "Frequently Ordered" => @food_items }
     render :index
@@ -25,6 +24,7 @@ class CategoriesController < AdminController
 
   def init
     authorize! :read, current_kitchen
+    return redirect_to root_url if !current_kitchen
     @food_item_filter = FoodItemFilter.new(current_kitchen.food_items, food_item_filter_params)
     @food_items = @food_item_filter.result
     @food_items = @food_items.select("food_items.*, c.name as category_name, c.priority as category_priority, s.name as supplier_name, s.priority as supplier_priority")
